5bc416d4f659c8d97adf68ddbeef707073936844
Aryan568 <aryanpatidar730@gmail.com>
2024-02-18 22:11:54 +0530

    char length done

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,41 @@ function App() {
 
   return (
     <>
-      <h1 className="text-5xl text-center text-white">Password Generator</h1>
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-700'><h1 className='text-white text-center my-3'>Password Generator</h1>
+      <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+        <input 
+          type="text"
+          value={pass}
+          className='outline-none w-full py-1 px-3'
+          placeholder='password'
+          readOnly 
+        />
+        <button className='outline-none bg-blue-700 px-3 py-0.5 text-white shrink-0'>Copy</button>
+
+      </div>
+      <div className='flex text-sm gap-x-2'>
+        <div className='flex items-center gap-x-1'>
+          <input 
+          type="range"
+          min={6}
+          max={100}
+          value={length}
+          className='cursor-pointer'
+          onChange={(e)=>{setLength(e.target.value)}}
+          />  
+          <label>Length: {length}</label>
+        </div>
+        <div className='flex items-center gap-x-1'>
+          <input 
+          type="checkbox"
+          defaultChecked={numberAllowed}
+          id="numberInput"
+          onChange={()=>{
+            setNumberAllowed((prev)=> !prev);
+          }} />
+        </div>
+      </div>
+      </div>
     </>
   )
 }
